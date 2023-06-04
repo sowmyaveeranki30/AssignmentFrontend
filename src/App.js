7e09af73ad819a5f44a0316c88ccbc6b4f21a893
sowmyaveeranki30 <veerankisowmya30@gmail.com>
2023-06-04 11:06:39 -0500

    Added details for the frontend of the assignment

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import BatteryLayout from './BatteryLayout';
+import { Container, Typography, TextField, Grid } from '@mui/material';
+import megapack2XL from "./assets/images/megapack2xl.jpeg";
+import megapack_2 from "./assets/images/megapack_2.jpeg";
+import megapack from "./assets/images/megapack.jpeg";
+import power_pack from "./assets/images/power_pack.jpeg";
+import transformers from "./assets/images/transformers.jpeg";
+
+const App = () => {
+  let totalPrice = 0;
+  let totalLandSize = 0;
+  let totalEnergy = 0;
+  let industrialBatteries = 0;
+  const [batterys, setBatterys] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_PATH}/batteries`);
+      setBatterys(response.data.data.map((preValue) => {
+        return {
+          ...preValue,
+          image: preValue.deviceName === "megapack2XL" && megapack2XL || preValue.deviceName === "megapack2" && megapack_2 || preValue.deviceName === "megapack" && megapack || preValue.deviceName === "powerpack" && power_pack || preValue.deviceName === "transformer" && transformers
+        }
+      }));
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const updateData = (id, updatedData) => {
+    axios.put(`${process.env.REACT_APP_API_PATH}/updateCount/${id}`, { count: updatedData })
+      .then(() => {
+        // Handle success
+        fetchData();
+      })
+      .catch(error => {
+        // Handle error
+        console.error(error);
+      });
+  };
+
+  return (
+    <Container>
+      <Typography variant="h4" sx={{ textAlign: "center", mb: 5, color: "red" }}>
+        Tesla Industrial Energy Battery Site
+      </Typography>
+      < Grid container spacing={2} sx={{ mb: 5 }}>
+        {batterys.map((item, index) => (
+          <Grid item xs={2.4} key={index}>
+            <TextField
+              id="outlined-basic"
+              label={item.name}
+              variant="outlined"
+              type="number"
+              value={item.count}
+              onChange={(e) => {
+                const count = parseInt(e.target.value)
+                updateData(item.id, count)
+              }}
+            />
+          </Grid>
+        ))}
+      </Grid >
+      <BatteryLayout
+        batterys={batterys}
+        updateData={updateData}
+        totalPrice={totalPrice}
+        totalLandSize={totalLandSize}
+        totalEnergy={totalEnergy}
+        industrialBatteries={industrialBatteries}
+      />
+    </Container >
+  );
+};
+
+export default App;
---
+++ src/BatteryLayout.js
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import { Typography, Grid, Box, CardMedia, CardContent, Card, Button } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import Alert from '@mui/material/Alert';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+const BatteryLayout = ({
+    batterys,
+    updateData,
+    totalPrice,
+    totalLandSize,
+    totalEnergy,
+    industrialBatteries
+}) => {
+    const [open, setOpen] = React.useState(true);
+    return (
+        <>
+            <Typography variant="h6" sx={{ textAlign: "center", mb: 3 }}>
+                Battery Layout
+            </Typography>
+            < Grid container spacing={2} sx={{ mb: 3 }}>
+                {batterys.map((item, index) => {
+                    totalPrice += item.count * item.cost;
+                    totalLandSize += item.count * parseInt(item.dimension.split('FT')[0]);
+                    totalEnergy += item.count * item.energy;
+                    industrialBatteries = industrialBatteries + item.count;
+                    return (
+                        < Grid item xs={4} key={index} >
+                            <Card sx={{ display: "flex" }}>
+                                <Box sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
+                                    <CardContent sx={{ flex: "1 0 auto" }}>
+                                        <Typography
+                                            variant="body1"
+                                            color="black"
+                                            component="div"
+                                        >
+                                            Name: {item.name}
+                                        </Typography>
+                                        <Typography
+                                            variant="body2"
+                                            color="text.secondary"
+                                            component="div"
+                                        >
+                                            Dimension: {item.dimension}
+                                        </Typography>
+                                        <Typography
+                                            variant="body2"
+                                            color="text.secondary"
+                                            component="div"
+                                        >
+                                            Energy: {item.energy}
+                                        </Typography>
+                                        <Typography
+                                            variant="body2"
+                                            color="text.secondary"
+                                            component="div"
+                                        >
+                                            Cost: ${item.cost}
+                                        </Typography>
+                                        <Typography
+                                            variant="body2"
+                                            color="text.secondary"
+                                            component="div"
+                                        >
+                                            ReleaseDate: {item.releaseDate}
+                                        </Typography>
+                                        <Box>
+                                            <Button variant="text"
+                                                onClick={() => {
+                                                    if (item.count > 0) {
+                                                        const increment = parseInt(item.count) - 1
+                                                        updateData(item.id, increment);
+                                                    }
+                                                }}
+                                            >
+                                                <RemoveIcon />
+                                            </Button>
+                                            {item.count}
+                                            <Button variant="text"
+                                                onClick={() => {
+                                                    const decrement = parseInt(item.count) + 1
+                                                    updateData(item.id, decrement);
+                                                }}
+                                            >
+                                                <AddIcon />
+                                            </Button>
+                                        </Box>
+                                    </CardContent>
+                                </Box>
+                                <CardMedia
+                                    component="img"
+                                    sx={{ width: 151 }}
+                                    image={item.image}
+                                    alt="Megapack 2XL"
+                                />
+                            </Card>
+                        </Grid>
+                    )
+                })}
+            </Grid >
+            <Box>
+                <Typography variant="body1" color="black" component="div">
+                    Total Price: ${totalPrice}
+                </Typography>
+                <Typography variant="body1" color="black" component="div">
+                    Total Land Size: {totalLandSize}FT
+                </Typography>
+                <Typography variant="body1" color="black" component="div">
+                    Total Energy: {totalEnergy}MWh
+                </Typography>
+            </Box>
+            {totalLandSize > 100 &&
+                <Alert severity="warning" sx={{ position: "fixed", top: 0 }}>site layouts should not exceed 100ft in width</Alert>
+            }
+            {industrialBatteries >= 4 &&
+                <Dialog
+                    open={open}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            As you bought four Industrial batteries, One transformer is needed
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={() => setOpen(false)} autoFocus>
+                            OK
+                        </Button>
+                    </DialogActions>
+                </Dialog>}
+        </>
+    );
+};
+
+export default BatteryLayout;
