7f152f1b0658fcd058609087cdf533f32f04abe2
sowmyaveeranki30 <veerankisowmya30@gmail.com>
2023-06-25 14:54:17 -0500

    Changed the port from 5000 to 3000 & in package.json set to export

--- src/SiteLayout.js
+++ src/SiteLayout.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import { Grid, Box, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+
+const ElementMapper = ({ item }) => {
+    const elements = [];
+    for (let i = 0; i < item.count; i++) {
+        elements.push(item.count > 0 && item.deviceName === "megapack2XL" && < Box sx={{ background: "gray", width: "300px", height: "50px", m: 1 }} key={i}></Box >);
+        elements.push(item.count > 0 && item.deviceName === "megapack2" && < Box sx={{ background: "gray", width: "220px", height: "50px", m: 1 }} key={i}></Box>);
+        elements.push(item.count > 0 && item.deviceName === "megapack" && < Box sx={{ background: "gray", width: "220px", height: "50px", m: 1 }} key={i}></Box>);
+        elements.push(item.count > 0 && item.deviceName === "powerpack" && < Box sx={{ background: "gray", width: "110px", height: "50px", m: 1 }} key={i}></Box>);
+        elements.push(item.count > 0 && item.deviceName === "transformer" && < Box sx={{ background: "gray", width: "110px", height: "50px", m: 1 }} key={i}></Box>);
+    }
+    return < Box sx={{ display: "flex" }}>{elements}</Box>
+};
 
 const SiteLayout = ({ batterys }) => {
     return (
@@ -7,29 +19,15 @@ const SiteLayout = ({ batterys }) => {
             <Typography variant="h6" sx={{ textAlign: "center", mb: 3, mt: 3 }}>
                 Site Layout
             </Typography>
-            < Grid container spacing={2} sx={{ mb: 3, border: "2px solid gray" }}>
+            < Box sx={{ mb: 3, border: "2px solid gray", display: "flex", flexWrap: "wrap", p: 1 }}>
                 {batterys.map((item, index) => (
                     (
-                        <Grid item key={index} sx={{ m: 1 }}>
-                            {item.count > 0 && item.deviceName === "megapack2XL" &&
-                                < Box sx={{ background: "gray", width: "300px", height: "50px", mt: 1 }}></Box>
-                            }
-                            {item.count > 0 && item.deviceName === "megapack2" &&
-                                < Box sx={{ background: "gray", width: "220px", height: "50px", mt: 1 }}></Box>
-                            }
-                            {item.count > 0 && item.deviceName === "megapack" &&
-                                < Box sx={{ background: "gray", width: "220px", height: "50px", mt: 1 }}></Box>
-                            }
-                            {item.count > 0 && item.deviceName === "powerpack" &&
-                                < Box sx={{ background: "gray", width: "110px", height: "50px", mt: 1 }}></Box>
-                            }
-                            {item.count > 0 && item.deviceName === "transformer" &&
-                                < Box sx={{ background: "gray", width: "110px", height: "50px", mt: 1 }}></Box>
-                            }
-                        </Grid>
+                        <Box key={index}>
+                            <ElementMapper item={item} />
+                        </Box>
                     )))
                 }
-            </Grid >
+            </Box >
         </>
     )
 }
